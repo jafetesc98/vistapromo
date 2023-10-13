8038cb04ed183548636d63f82f6ec97fe0c36342
promocionesycombos <jafet_esc98@hotmail.com>
2023-10-13 09:23:43 -0600

    vistapromo modificado en fechas y impresion

--- assets/CardBoxComponentEmpty.e4601229.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as e,c as o,a as s}from"./index.1f2e837b.js";const c={},a={class:"text-center py-24 text-gray-500 dark:text-slate-400"},n=s("p",null,"Sin datos registrados",-1),r=[n];function _(d,i){return e(),o("div",a,r)}const l=t(c,[["render",_]]);export{l as C};
